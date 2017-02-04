c574b5a462ae95b849c4cf66d829cda2c0094296
Vincent Gutierrez <vincentgg89@gmail.com>
2017-02-04 15:49:18 -0600

    Implement create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,25 @@ class GamesController < ApplicationController
   def new
     @users = User.all_except_me(current_user)
   end
+
+  def create
+    @game = Game.create!(game_params)
+    @opponent = User.find(params["opponent"]["id"])
+    @opponent.players.create!(game: @game, score: opponent_params[:score])
+    current_user.players.create!(game: @game, score: player_params[:score])
+    redirect_to root_url
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:date)
+    end
+
+    def opponent_params
+      params.require(:opponent).permit(:score)
+    end
+
+    def player_params
+      params.require(:player).permit(:score)
+    end
 end
