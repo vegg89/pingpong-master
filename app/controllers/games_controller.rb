a6284e6296c0837b0d3578becc56ce367a93fd4d
Vincent Gutierrez <vincentgg89@gmail.com>
2017-02-06 15:40:22 -0600

    Update ratings when a game is logged

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require 'elo'
+
 class GamesController < ApplicationController
   def index
     @games = current_user.games
@@ -12,6 +14,7 @@ class GamesController < ApplicationController
     @opponent = User.find(params["opponent"]["id"])
     @opponent.players.create!(game: @game, score: opponent_params[:score])
     current_user.players.create!(game: @game, score: player_params[:score])
+    update_ratings
     redirect_to root_url
   end
 
@@ -26,5 +29,24 @@ class GamesController < ApplicationController
 
     def player_params
       params.require(:player).permit(:score)
+    end
+
+    def update_ratings
+      p1_res = current_user.players.where(game: @game).first
+      p2_res = @opponent.players.where(game: @game).first
+      res = calculate_result(p1_res, p2_res)
+      Match.new(current_user, @opponent, res)
+      current_user.save!
+      @opponent.save!
+    end
+
+    def calculate_result(p1, p2)
+      if p1.score > p2.score
+        1
+      elsif p1.score == p2.score
+        0.5
+      else
+        0
+      end
     end
 end
